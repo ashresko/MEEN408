4959a590f324df645fa9bfdd9f71d92c0a1cfed5
blakeleiker <blakealeiker@gmail.com>
2016-12-09 17:56:30 -0600

    Added Comments to ADC Class (ADC.h and ADC.cpp) in the project.

--- ProjectCode/ADC/ADC.cpp
+++ ProjectCode/ADC/ADC.cpp
@@ -8,47 +8,33 @@ ADC::ADC(int ADCPinNumberr) {
   // Set filename strings
   std::stringstream ss;
   ss << "/sys/bus/iio/devices/iio:device0/";  // ADCDeviceFile
-  ADCDeviceFile = ss.str();                   //
-  ss.clear();                                 //
-  ss.str(std::string());                      //
+  ADCDeviceFile = ss.str();                   // put the contents of the stringstream into the device string
+  ss.clear();                                 // clear any status flags on the stringstream
+  ss.str(std::string());                      // delete the contents of the stringstream so we can use it again
   ss << ADCDeviceFile << "in_voltage" << ADCPinNumber
      << "_raw";               // ADCVoltageFile
-  ADCVoltageFile = ss.str();  //
+  ADCVoltageFile = ss.str();  // see above for device file
   ss.clear();                 //
   ss.str(std::string());      //
 }
 
 double ADC::readVoltage() {
-  double maxADCVoltage = 1.8;  // maximum allowed voltage -- DO NOTE EXCEED
-  double minADCVoltage = 0.0;    // minimum measureable voltage (lowest output)
-  int maxRaw = 4095;           // maximum output from ADC
-  int minRaw = 0;              // minimum output from ADC
-  std::ifstream ifs;
-
-  int rawADC = 0;
-  ifs.open(ADCVoltageFile.c_str());
-  if (!(ifs.is_open())) {
-    std::cout << "Cannot get the ADC Voltage.\n";
-    // throw exception;
-  } else {
-    ifs >> rawADC;
-  }
-  ifs.close();
-  ADCVoltage = ((maxADCVoltage - minADCVoltage) / (maxRaw - minRaw)) * rawADC +  minADCVoltage;
+  int rawADC = readRaw(); //get the raw bit voltage reading from the adc pin using a function defined below
+  ADCVoltage = ((maxADCVoltage - minADCVoltage) / (maxRaw - minRaw)) * rawADC +  minADCVoltage; //convert to actual voltage from bit reading
   return ADCVoltage;
 }
 
 int ADC::readRaw() {
-  std::ifstream ifs;
-  int rawADC = 0;
-  ifs.open(ADCVoltageFile.c_str());
+  std::ifstream ifs;  // filestream variable used to read from voltage file
+  int rawADC = 0; //variable used to store direct reading
+  ifs.open(ADCVoltageFile.c_str()); //open the voltage file
   if (!(ifs.is_open())) {
     std::cout << "Cannot get the ADC Voltage.\n";
     // throw exception;
   } else {
-    ifs >> rawADC;
+    ifs >> rawADC;  //read the file voltage (in bits) into the temporary storage variable
   }
-  ifs.close();
-  return rawADC;
+  ifs.close(); //close the file
+  ADCRawReading = rawADC;  //put the reading in the class variable ADCRawReading -- not necessary, but done
+  return rawADC; //return the voltage reading
 }
-
--- ProjectCode/ADC/ADC.h
+++ ProjectCode/ADC/ADC.h
@@ -11,7 +11,12 @@ class ADC {
  private:
   // ADC parameters
   double ADCVoltage;
+  int ADCRawReading;
   int ADCPinNumber;
+  int maxRaw = 4095;  //maximum returned bit reading
+  int minRaw = 0;  //minimum returned bit reading
+  double maxADCVoltage = 1.8;  // maximum allowed voltage -- DO NOT EXCEED
+  double minADCVoltage = 0.0;    // minimum measureable voltage (lowest output)
   // filename strings
   std::string ADCDeviceFile;
   std::string ADCVoltageFile;
