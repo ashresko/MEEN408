00ca89c2ecb768be2c876e0151af3097d52063cd
blakeleiker <blakeleiker@Blakes-MacBook-Pro.local>
2016-12-10 01:46:26 -0600

    Create SERVOMOTOR

---
+++ ProjectCode/SERVOMOTOR/SERVOMOTOR.h
@@ -0,0 +1,23 @@
+#ifndef SERVOMOTOR_H
+#define SERVOMOTOR_H
+
+#include "PWM.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+
+class SERVOMOTOR {
+private:
+  PWM servoPWM;  // pwm which will send control signal to servomotor
+
+public:
+  SERVOMOTOR();
+  ~SERVOMOTOR();
+  int getAngle();
+  void setAngle();
+
+};
+
+#endif
