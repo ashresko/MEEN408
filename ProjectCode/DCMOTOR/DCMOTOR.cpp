05ab4cc3bcde449438626f9e04cedecf5b006985
augustusellis <augustusellis@gmail.com>
2016-12-10 13:47:51 -0600

    Updated DC motor

--- ProjectCode/DCMOTOR/DCMOTOR.cpp
+++ ProjectCode/DCMOTOR/DCMOTOR.cpp
@@ -5,6 +5,8 @@ DCMOTOR::DCMOTOR(int PWMNumberr, int EQEPNumberr)
   // the commands above with : pwmconstructor, eqepconstructor are called an
   // initializer list. We used to initialize the member variables, especially
   // when they are a instances of our own classes
+  setk_t(0);
+  setk_emf(0);
   // We now unenable the motor
   enable(0);
   // and are done with the constructor.
@@ -24,10 +26,15 @@ void DCMOTOR::setPWMDutyCycle(int DutyCyclee) {
 int DCMOTOR::getPWMDutyCycle() {
   return motorPWM.getPWMDutyCycle(); // same as getPWMPeriod above
 }
+double setk_emf(double k_emff) { k_emf = k_emff; }
+double getk_emf() { return k_emf; }
+double setk_t(double k_tt) { k_t = k_tt; }
+double getk_t() { return k_t; }
 void DCMOTOR::enable(int enablee) {
-  if (enablee != 0) { // so long as the input is not zero, we accept it enable
+  if (enablee != 0) { // so long as the input is not zero, we accept it as enable
     enablee = 1;
   }
   motorPWM.enable(enablee);
 }
 }
+-*
--- ProjectCode/DCMOTOR/DCMOTOR.h
+++ ProjectCode/DCMOTOR/DCMOTOR.h
@@ -14,7 +14,7 @@ private:
   // Motor Parameters
   PWM motorPWM;   // pwm to do basic control signal to motor
   EQEP motorEQEP; // for reading back the angle
-  double gear_r;  // the motor's gear ratio
+  double gear_r;  // the motor's gear ratio. Not sure how to use this right now.
   double k_emf;   // emf motor constant  w = k_emf * Voltage
   double k_t;     // torque constant     Torque = k_t * current
   // Angle Tracking
@@ -30,6 +30,10 @@ public:
   int getPWMPeriod();
   void setPWMDutyCycle(int DutyCyclee);
   int getPWMDutyCycle();
+  double setk_emf(double k_emff);
+  double getk_emf();
+  double setk_t(double k_tt);
+  double getk_t();
   void enable(int enablee); // disables or enables the motors
 };
 
