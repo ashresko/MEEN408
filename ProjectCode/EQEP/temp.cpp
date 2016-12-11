c9e4a3767e181e8c24511b9df771705b1928a15a
blakeleiker <blakealeiker@gmail.com>
2016-12-10 18:00:44 -0600

    Added EQEP test file

--- ProjectCode/EQEP/temp.cpp
+++ ProjectCode/EQEP/temp.cpp
@@ -1,12 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include <unistd.h>
 #include "EQEP.h"
 
 int main()
 {
 	EQEP myEQEP(0);
 	std::cout << "EQEP Start" << std::endl;
-	for (int i = 0; i < 30; i++){
+	for (int i = 0; i < 100; i++){
 		std::cout << "(Angle) : (" << myEQEP.readPosition() << ")" << std::endl;
+		usleep(100000);
 	}
 	std::cout << "EQEP End" << std::endl;
 	return 0;
